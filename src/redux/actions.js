836d3a40963b83fe2ba38e76ffb20cefef89dc58
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2021-05-15 22:28:46 +0900

    Add async action to import TODO

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,19 @@ import { ADD_TODO, TOGGLE_TODO, SET_FILTER } from "./actionTypes";
 
 let nextTodoId = 0;
 
+const createImportedContent = (activity, link) =>
+  [
+    activity,
+    link ? `(${link})` : '',
+  ].join(' ');
+
+export const importTodo = content => dispatch =>
+  fetch('https://www.boredapi.com/api/activity/')
+  .then(response => response.json())
+  .then(({ activity, link }) => dispatch(
+    addTodo(createImportedContent(activity, link))
+  ));
+
 export const addTodo = content => ({
   type: ADD_TODO,
   payload: {
